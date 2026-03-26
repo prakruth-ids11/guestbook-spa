ec2a2908b9bffecc4032ec0a0051bf452f6a9c3c
prakruth-ids11 <prakruthids11@gmail.com>
2026-03-26 21:11:41 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs'); // to save messages
+
+const app = express();
+const PORT = 3000;
+
+// parse JSON and URL-encoded data
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// serve static files (your HTML, CSS, JS)
+app.use(express.static('public'));
+
+// save messages to a JSON file
+app.post('/submit', (req, res) => {
+    const { name, message } = req.body;
+    if (!name || !message) return res.status(400).send('Name or message missing!');
+
+    let data = [];
+    if (fs.existsSync('messages.json')) {
+        data = JSON.parse(fs.readFileSync('messages.json'));
+    }
+
+    data.push({ name, message });
+    fs.writeFileSync('messages.json', JSON.stringify(data, null, 2));
+
+    res.send('Success');
+});
+app.get('/messages', (req, res) => {
+    if (fs.existsSync('messages.json')) {
+        const data = JSON.parse(fs.readFileSync('messages.json'));
+        res.json(data);
+    } else {
+        res.json([]);
+    }
+});
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
